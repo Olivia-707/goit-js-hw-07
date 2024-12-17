5907f48cdbedc702afba3673eccc6d1e51498dcc
Olivia-707 <oliviapenkovskaya@gmail.com>
2024-12-17 10:41:09 +0200

    fix

--- js/task-1.js
+++ js/task-1.js
@@ -4,6 +4,6 @@ console.log(`Number of categories: ${items.length}`);
 for (const item of items) {
   const category = item.querySelector('.list-title');
   console.log(`Category: ${category.textContent}`);
-  const subItems = item.querySelectorAll('.sub-item');
+  const subItems = item.querySelectorAll('.list-item');
   console.log(`Elements: ${subItems.length}`);
 }
